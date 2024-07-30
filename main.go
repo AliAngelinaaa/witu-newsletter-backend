8d6d4240306c252f00bf0518249a8c35739e0c21
nafisaanzum2003 <nafisa.anzum@nytimes.com>
2024-07-30 16:20:10 -0400

    had to change parts of it

--- main.go
+++ main.go
@@ -1,20 +1,53 @@
+// package main
+
+// import (
+// 	"github.com/aliangelinaaa/witu-newsletter-backend/controllers"
+// 	"github.com/aliangelinaaa/witu-newsletter-backend/models"
+// 	"github.com/gin-gonic/gin"
+// )
+// func main() {
+	
+// 	router := gin.Default()
+
+// 	models.ConnectDatabase()
+
+// 	router.POST("/posts", controllers.CreatePost)
+// 	router.GET("/posts", controllers.FindPosts)
+// 	router.GET("/posts/:id", controllers.FindPost)
+// 	router.PATCH("/posts/:id", controllers.UpdatePost)
+// 	router.DELETE("/posts/:id", controllers.DeletePost)
+
+// 	router.Run("localhost:8080")
+// }
+
+
 package main
 
 import (
-	"github.com/aliangelinaaa/witu-newsletter-backend/controllers"
-	"github.com/aliangelinaaa/witu-newsletter-backend/models"
-	"github.com/gin-gonic/gin"
+    "github.com/aliangelinaaa/witu-newsletter-backend/controllers"
+    "github.com/aliangelinaaa/witu-newsletter-backend/models"
+    "github.com/gin-contrib/cors"
+    "github.com/gin-gonic/gin"
 )
+
 func main() {
-	router := gin.Default()
+    router := gin.Default()
 
-	models.ConnectDatabase()
+    // Setup CORS
+    router.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"http://localhost:5173"}, // Allow only your front-end URL
+        AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+        AllowCredentials: true,
+    }))
 
-	router.POST("/posts", controllers.CreatePost)
-	router.GET("/posts", controllers.FindPosts)
-	router.GET("/posts/:id", controllers.FindPost)
-	router.PATCH("/posts/:id", controllers.UpdatePost)
-	router.DELETE("/posts/:id", controllers.DeletePost)
+    models.ConnectDatabase()
 
-	router.Run("localhost:8080")
+    router.POST("/posts", controllers.CreatePost)
+    router.GET("/posts", controllers.FindPosts)
+    router.GET("/posts/:id", controllers.FindPost)
+    router.PATCH("/posts/:id", controllers.UpdatePost)
+    router.DELETE("/posts/:id", controllers.DeletePost)
+
+    router.Run("localhost:8080")
 }
